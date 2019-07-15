1f212f7bcf47da2424cf2f2c6b0a8b7985a76134
escouvois <bastien.escouvois@gmail.com>
2019-07-15 11:01:12 +0100

    Datastructures - Tree (fix + main)

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -21,19 +21,19 @@ object Tree {
   def depth(t: Tree[Int]): Int =
     t match {
       case Leaf(_) => 0
-      case Branch(l, r) => 1 + depth(l) max depth(r)
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
     }
 
-  def map[A, B](t: Tree[A])(implicit f: A => B): Tree[B] =
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
     t match {
       case Leaf(x) => Leaf(f(x))
-      case Branch(l, r) => Branch(map(l), map(r))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
     }
 
   def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B =
     t match {
       case Leaf(x) => f(x)
-      case Branch(l, r) => g(fold(l), fold(r))
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
     }
 
   def sizeFold[A](t: Tree[A]): Int =
@@ -47,4 +47,23 @@ object Tree {
 
   def mapFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
     fold(t)(x => Leaf(f(x)): Tree[B])(Branch(_, _))
+
+  def main(args: Array[String]): Unit = {
+    val t =
+      Branch(
+        Branch(
+          Branch(Leaf(1), Leaf(2)),
+          Branch(Leaf(13), Leaf(4))),
+        Branch(
+          Branch(Leaf(5),
+            Branch(Leaf(15), Leaf(9))
+          ),
+          Branch(Leaf(12), Leaf(8))
+        )
+      )
+    println(s"size: ${size(t)}")
+    println(s"maximum: ${maximum(t)}")
+    println(s"depth: ${depth(t)}")
+    println(s"map: ${map(t)(1 + _)}")
+  }
 }
