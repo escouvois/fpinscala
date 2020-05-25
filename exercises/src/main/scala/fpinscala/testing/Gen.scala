43f16af2565b264fa6aa8615e051a982851f8d78
bescouvois <bastien.escouvois@voxaly.com>
2020-05-25 22:36:13 +0200

    Property based testing : 8.3

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,35 +1,200 @@
 package fpinscala.testing
 
 import fpinscala.laziness.Stream
+import fpinscala.state.RNG.Simple
 import fpinscala.state._
-import fpinscala.parallelism._
-import fpinscala.parallelism.Par.Par
-import Gen._
-import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import fpinscala.testing.Prop._
+
+import scala.language.{implicitConversions, postfixOps}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
+/*trait Prop {
+  def check: Boolean
+
+  def &&(p: Prop): Prop = new Prop {
+    override def check: Boolean = Prop.this.check && p.check
+  }
+}*/
+
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+  def &&(p: Prop): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Passed | Proved => p.run(max, n, rng)
+        case x => x
+      }
+  }
+
+  def ||(p: Prop): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Falsified(failure, _) => p.tag(failure).run(max, n, rng)
+        case x => x
+      }
+  }
+
+  def tag(failedCase: String): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Falsified(e, c) => Falsified(failedCase + "\n" + e, c)
+        case x => x
+      }
+  }
 }
 
 object Prop {
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  type SuccessCount = Int
+  type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+
+  case class Falsified(failure: FailedCase,
+                       successes: SuccessCount) extends Result {
+    def isFalsified = true
+  }
+
+  case object Proved extends Result {
+    def isFalsified = false
+  }
+
+
+  /* Produce an infinite random stream from a `Gen` and a starting `RNG`. */
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) =>
+      randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+        case (a, i) => try {
+          if (f(a)) Passed else Falsified(a.toString, i)
+        } catch {
+          case e: Exception => Falsified(buildMsg(a, e), i)
+        }
+      }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    forAll(g(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max,n,rng) =>
+      val casesPerSize = (n - 1) / max + 1
+      val props: Stream[Prop] =
+        Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop =
+        props.map(p => Prop { (max, n, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+      prop.run(max,n,rng)
+  }
+
+  // String interpolation syntax. A string starting with `s"` can refer to
+  // a Scala value `v` as `$v` or `${v}` in the string.
+  // This will be expanded to `v.toString` by the Scala compiler.
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def apply(f: (TestCases, RNG) => Result): Prop =
+    Prop { (_, n, rng) => f(n, rng) }
+
+  def main(args: Array[String]): Unit = {
+    val rng = Simple(System.currentTimeMillis)
+    val genString = Gen.string(5)
+    val propA = forAll(genString)(s => s.substring(4) == s.charAt(4).toString)
+    val propB = forAll(genString)(s => s.startsWith(s) == true)
+    println("and " + (propA && propB).run(100, 100, rng))
+  }
+}
+
+case class Gen[+A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] =
+    Gen(sample.map(f))
+
+  def flatmap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(f(_).sample))
+
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size flatmap (listOfN(_))
+
+  def unsized: SGen[A] = SGen(_ => this)
+
+  def map2[B, C](g: Gen[B])(f: (A, B) => C): Gen[C] =
+    Gen(sample.map2(g.sample)(f))
+
+  def **[B](g: Gen[B]): Gen[(A, B)] =
+    this.map2(g)((_, _))
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.nonNegativeInt).map(n => n % 2 == 0))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+
+  def string(n: Int): Gen[String] =
+    listOfN(n, choose(97, 123).map(_.toChar)).map(_.mkString)
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatmap(if (_) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double).flatMap(d => if (d < threshold) g1._1.sample else g2._1.sample))
+  }
+
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(listOfN(_, g))
+
+  def main(args: Array[String]): Unit = {
+    val rng = Simple(42)
+    println("choose " + choose(0, 10).sample.run(rng)._1)
+    println("boolean " + boolean.sample.run(rng)._1)
+    println("listOfN " + listOfN(10, choose(0, 10)).sample.run(rng)._1)
+    println("string " + string(10).sample.run(rng)._1)
+  }
+
 }
 
-trait Gen[A] {
+
+/*trait Gen[A] {
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+}*/
+
+case class SGen[+A](forSize: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = forSize(n)
+
+  def map[B](f: A => B): SGen[B] = SGen(forSize(_) map f)
+
+  def flapmap[B](f: A => SGen[B]): SGen[B] = SGen { n =>
+    forSize(n) flatmap {
+      f(_).forSize(n)
+    }
+  }
+
+  def **[B](s2: SGen[B]): SGen[(A, B)] =
+    SGen { n => apply(n) ** s2(n) }
 }
-
-trait SGen[+A] {
-
-}
-
